dbb770881e4b9435b69a0cd2222e322097560bf2
Alan Wang <wy.dong96@gmail.com>
2023-04-25 14:12:16 +0700

    add multiplier2 and test of it

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  require: [
+    path.join(__dirname, 'node_modules', 'esbuild-register'),
+  ],
+  extension: ['ts'],
+  spec: 'circuits/*/test/*.ts',
+};
