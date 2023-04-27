96eb1d51bf410212c6534f3c99098d4866e5c63f
Alan Wang <wy.dong96@gmail.com>
2023-04-27 20:23:14 +0700

    add zkblind circuit

--- circuits/ecsda-circom/test/sig_verify.ts
+++ circuits/ecsda-circom/test/sig_verify.ts
@@ -1,91 +1,13 @@
 import path from "path";
 import wasm_tester from "../../../wasm_tester";
-import { setupDirectories } from "../../test_utils"
-import { hmac } from '@noble/hashes/hmac';
-import { sha256 } from '@noble/hashes/sha256';
+import {
+  setupDirectories,
+  getCircuitInputWithAddrAndSig,
+  getAddrAndSig
+} from "../../test_utils"
 import { expect } from 'chai';
-import { privateToAddress } from "@ethereumjs/util";
-import * as secp256k1 from '@noble/secp256k1';
-import { sha256 as jsSha256 } from 'js-sha256';
 
 const pathToCircom = "./sig_verify.circom"
-
-function bigint_to_array(n: number, k: number, x: bigint) {
-  let mod: bigint = 1n;
-  for (var idx = 0; idx < n; idx++) {
-    mod = mod * 2n;
-  }
-
-  let ret: bigint[] = [];
-  var x_temp: bigint = x;
-  for (var idx = 0; idx < k; idx++) {
-    ret.push(x_temp % mod);
-    x_temp = x_temp / mod;
-  }
-  return ret;
-}
-
-function getCircuitInputWithAddrAndSig(input: string) {
-  const [msg, sigHex]: [string, string] = JSON.parse(input)
-
-  const sig = secp256k1.Signature.fromCompact(sigHex).addRecoveryBit(0)
-
-  const msgHash = jsSha256(msg)
-
-  const msgHashBigInt = BigInt("0x" + msgHash)
-
-  const recoveredPublicKey = sig.recoverPublicKey(msgHash); // Public key recovery
-
-  return getCircuitInput(
-    sig.r,
-    sig.s,
-    msgHashBigInt,
-    recoveredPublicKey.x,
-    recoveredPublicKey.y
-  )
-}
-
-function getCircuitInput(
-  r_bigint: bigint,
-  s_bigint: bigint,
-  msghash_bigint: bigint,
-  pub0: bigint,
-  pub1: bigint,
-) {
-  var r_array: bigint[] = bigint_to_array(64, 4, r_bigint);
-  var s_array: bigint[] = bigint_to_array(64, 4, s_bigint);
-  var msghash_array: bigint[] = bigint_to_array(64, 4, msghash_bigint);
-  var pub0_array: bigint[] = bigint_to_array(64, 4, pub0);
-  var pub1_array: bigint[] = bigint_to_array(64, 4, pub1);
-
-  return {
-    "r": r_array,
-    "s": s_array,
-    "msghash": msghash_array,
-    "pubkey": [pub0_array, pub1_array]
-  }
-}
-
-// example output of this function:
-// ["0xefcbe272b0febe3edadc034af7a3f53ed35aaa53","9ea2c44a6c411cddd0351db03d87b127b64fd420104e0e072d39b29f6318e6302e6f6390a4f17f3f77a83c7b12aa692c1e6fae925a399e0800afe9de71ebab7c"]
-function getAddrAndSig(privateKey: string) {
-  const privKeyBuffer = Buffer.from(privateKey, "hex");
-  const addr = "0x" + privateToAddress(privKeyBuffer).toString("hex");
-  console.log("ETH Address:" + addr)
-
-  const addrHash = jsSha256(addr)
-  console.log("getAddrAndSig addrHash: ", addrHash)
-  secp256k1.etc.hmacSha256Sync = (k, ...m) => hmac(sha256, k, secp256k1.etc.concatBytes(...m))
-  const addrSig = secp256k1.sign(addrHash, privateKey).toCompactHex()
-
-  const result = [
-    addr,
-    addrSig
-  ]
-
-  return JSON.stringify(result)
-}
-
 
 describe("Test sig verify", function () {
   const output = setupDirectories(pathToCircom);
@@ -109,5 +31,4 @@ describe("Test sig verify", function () {
 
     await circuit.checkConstraints(w);
   });
-
 });
