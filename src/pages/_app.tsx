bb33e8ad5825739a62bbc4748af982d09998d4e1
pfed-prog <fedorovn19@gmail.com>
2023-04-28 13:54:35 +0700

    fixed bug on navbar thx to merlin ai

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
-
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
